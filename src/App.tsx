f0f771fca42643555e458df2fd03d5d7c2a020a2
Ibrahim <ibz_374@hotmail.com>
2025-05-16 00:26:31 +0100

    Integrated Supabase and GoogleOAuth and configure basic session handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Auth } from '@supabase/auth-ui-react';
+import { supabase } from '../supabaseClient';
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import type { Session } from '@supabase/supabase-js';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [session, setSession] = useState<Session | null>(null); //session can either be a Supabase Session object(when logged in) or null (when logged out)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Failed to sign out", error)
+    }
+  }
+
+  if (!session) {
+    return (
+      <>
+        <Auth supabaseClient={supabase} providers={['google']} appearance={{ theme: ThemeSupa }} />
+      </>
+    )
+  } else {
+    return (
+    <div>
+      <h2>Welcome {session?.user?.user_metadata?.full_name}.</h2>
+      <button onClick={signOut}>Sign out</button>
+    </div>
   )
+  }
 }
 
 export default App
